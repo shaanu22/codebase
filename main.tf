11fe91b6b6773ceb081c557e1dea5359a6f880b3
shaanu22 <mails2samtaiwo@gmail.com>
2023-08-15 00:51:34 -0500

    edited some variables

--- main.tf
+++ main.tf
@@ -1,30 +1,27 @@
-data "aws_vpc" "app_vpc" {
+resource "aws_vpc" "app_vpc" {
   cidr_block = var.vpc_cidr
 
-  filter {
-    name   = "tag:Name"
-    values = ["App VPC"]
+  tags = {
+    Name = "Main VPC"
   }
 }
 
-data "aws_subnet" "subnet_1" {
+resource "aws_subnet" "subnet_1" {
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = var.subnet_1_cidr
   availability_zone = element(var.availability_zones, 0)
 
-  filter {
-    name   = "tag:Name"
-    values = ["var.subnet_1_tag"]
+  tags = {
+    Name = "var.subnet_1_tag"
   }
 }
 
-data "aws_subnet" "subnet_2" {
+resource "aws_subnet" "subnet_2" {
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = var.subnet_2_cidr
   availability_zone = element(var.availability_zones, 1)
 
-  filter {
-    name   = "tag:Name"
-    values = ["var.subnet_2_tag"]
+  tags = {
+    Name = "var.subnet_1_tag"
   }
 }
